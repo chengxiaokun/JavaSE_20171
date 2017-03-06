1fa14db91162ecf7fd3d1b58ff4500e75d005da4
mingfei <mingfei.net@gmail.com>
2017-03-06 14:52:24 +0800

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,14 @@
+/**
+ * Created by mingfei.net@gmail.com
+ * 3/6/17 14:22
+ */
+public class Hello {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) { // main 主方法 arguments 参数
+        System.out.println("你好。。。\"\"");
+    }
+}
