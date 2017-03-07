f9833d1b284f49f3b12a6e204683bd1102026c55
mingfei <mingfei.net@gmail.com>
2017-03-07 15:57:36 +0800

    Update

---
+++ src/main/java/BitWiseOperators.java
@@ -0,0 +1,24 @@
+/**
+ * Created by mingfei.net@gmail.com
+ * 3/7/17 15:39
+ */
+public class BitWiseOperators {
+    public static void main(String[] args) {
+        // Bit 位\ [bɪt] 比特 Byte 字节\ [baɪt] 1 Byte = 8 Bit
+        // 1KB = 1024 Byte MB = 1024KB GB TB PB
+        // 00000000
+        // 2^31 - 1  -2^31
+
+        int x = -1;
+        int y = 1;
+
+        System.out.println(x >>> y); // 按位与 按位左移
+
+
+        // 000..01   100
+        // 000..10
+        //      00
+        //      11
+        //      11
+    }
+}
