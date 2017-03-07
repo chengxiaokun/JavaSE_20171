6e5c435a950cbdf326eb51b03dd70af62647b182
mingfei <mingfei.net@gmail.com>
2017-03-07 16:53:42 +0800

    Update

--- src/main/java/BitWiseOperators.java
+++ src/main/java/BitWiseOperators.java
@@ -13,11 +13,11 @@ public class BitWiseOperators {
 
         int x = 1;
         int y = 1;
+//        0000...|1
+        System.out.println(x >> y); // 按位与 按位左移 无符号右移
+//        System.out.println(~x);
 
-//        System.out.println(x >>> y); // 按位与 按位左移
-        System.out.println(~x);
-
-        System.out.println(Integer.toBinaryString(-2));
+        System.out.println(Integer.toBinaryString(-1));
         // 000..01 ~ 111...10 -2
         // 000..10
         //      00
@@ -26,5 +26,10 @@ public class BitWiseOperators {
 
 //        2 -2
         // 000...10 111...01 111...10
+
+//      1  -1
+//        000...1 111...0 1111...|1
+
+//        [1001.....010100] <<
     }
 }
