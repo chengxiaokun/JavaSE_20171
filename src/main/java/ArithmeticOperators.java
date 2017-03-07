906b4e0f7c9039d38d82f5c96f58dc24e8dba507
mingfei <mingfei.net@gmail.com>
2017-03-07 11:39:16 +0800

    Update

---
+++ src/main/java/ArithmeticOperators.java
@@ -0,0 +1,22 @@
+/**
+ * Created by mingfei.net@gmail.com
+ * 3/7/17 11:32
+ */
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int x = 1; // integer
+        int y = 2;
+        int z = 5 / 7;
+
+        double d1 = 1;
+        double d2 = 2;
+
+        System.out.println(z);
+
+        System.out.println(d1 / d2);
+
+        System.out.println(x / d2);
+
+        System.out.println(d1 / y);
+    }
+}
---
+++ src/main/java/StringTest.java
@@ -0,0 +1,15 @@
+/**
+ * Created by mingfei.net@gmail.com
+ * 3/7/17 11:25
+ */
+public class StringTest {
+    public static void main(String[] args) {
+        String s1 = "1234.567";
+        String s2 = new String("abcd...");
+        String s3 = "abcd...";
+
+        String s4 = null;
+
+        System.out.println(s4);
+    }
+}
