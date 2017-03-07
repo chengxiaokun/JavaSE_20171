af3faa13c8297533227e8256444007638f8bcfa1
mingfei <mingfei.net@gmail.com>
2017-03-07 09:36:00 +0800

    Update

---
+++ src/main/java/PrimitiveDataType.java
@@ -0,0 +1,18 @@
+/**
+ * Created by mingfei.net@gmail.com
+ * 3/7/17 09:12
+ */
+public class PrimitiveDataType {
+    public static void main(String[] args) {
+        boolean b = true;
+
+        byte b1 = 1;
+        char c  = 2;
+        short s = 3;
+        int i = 4;
+        long l = 5;
+
+        float f = 1.1f; // 在出现错误时，要仔细看错误提示
+        double d = 2.2;
+    }
+}
---
+++ src/main/java/Public.java
@@ -0,0 +1,6 @@
+/**
+ * Created by mingfei.net@gmail.com
+ * 3/7/17 09:07
+ */
+public class Public {
+}
