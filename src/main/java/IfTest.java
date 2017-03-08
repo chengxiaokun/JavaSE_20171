befea598ddbf5e6179bbdc796ada11256439c6d1
mingfei <mingfei.net@gmail.com>
2017-03-08 16:04:31 +0800

    Update

---
+++ src/main/java/IfTest.java
@@ -0,0 +1,13 @@
+/**
+ * Created by mingfei.net@gmail.com
+ * 3/8/17 15:59
+ */
+public class IfTest {
+    public static void main(String[] args) {
+        int grade = 50; // n.\ 年级；等级；成绩；级别；阶段
+        if (grade >= 60) {
+            System.out.println("及格。");
+            System.out.println("测试。。。");
+        }
+    }
+}
