dcaff4ddfb1c89365f3b589869e8bdffa3873f49
mingfei <mingfei.net@gmail.com>
2017-03-08 08:30:09 +0800

    Update

--- src/main/java/AA.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by mingfei.net@gmail.com
- * 3/7/17 08:50
- */
-public class AA {
-}
-
-// Hello HowAreYou True False
--- src/main/java/Public.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by mingfei.net@gmail.com
- * 3/7/17 09:07
- */
-public class Public {
-}
