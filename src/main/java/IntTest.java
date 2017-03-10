b0c7bd5d6ceceb5ec15391aa99431e6ca687aded
mingfei <mingfei.net@gmail.com>
2017-03-10 17:19:23 +0800

    Update

---
+++ src/main/java/IntTest.java
@@ -0,0 +1,15 @@
+/**
+ * Created by mingfei.net@gmail.com
+ * 3/10/17 17:11
+ */
+public class IntTest {
+    public static void main(String[] args) {
+        int x = 0b1001_0100_0100_1010_0100; // b : binary 二进制\ ['baɪnərɪ]
+        System.out.println(x);
+
+//        boolean b = (12==0) & (1/0 < 1);
+//        System.out.println(b);
+
+        System.out.println(1d/0d); // Infinity n.\ 无穷；无限大；无限距
+    }
+}
