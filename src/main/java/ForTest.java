84b538cfff1189528863dddde63e1788db6675c5
mingfei <mingfei.net@gmail.com>
2017-03-10 10:13:33 +0800

    Update

--- src/main/java/ForTest.java
+++ src/main/java/ForTest.java
@@ -4,9 +4,16 @@
  */
 public class ForTest {
     public static void main(String[] args) {
-        for(int i = 0; i < 10; i++) { // 循环变量 循环的判断条件 循环变量的变化
-            System.out.println(i); // 0 1 2 ... 8 9
-        }
+//        for(int i = 0; i < 10; i++) { // 循环变量 循环的判断条件 循环变量的变化
+//            System.out.println(i); // 0 1 2 ... 8 9
+//        }
         // ...
+
+        for (long i = 0; i < Long.MAX_VALUE; i++)
+            ;
+        System.out.println("I Love My Girl!");
     }
 }
+/*
+for(int i=0; i<forever;i++);System.out.println("I Love My Girl!");
+ */
---
+++ src/main/java/WhileTest.java
@@ -0,0 +1,13 @@
+/**
+ * Created by mingfei.net@gmail.com
+ * 3/10/17 10:12
+ */
+public class WhileTest {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) { // conj.\ 虽然；然而；当……的时候
+            System.out.println(i);
+            i++;
+        }
+    }
+}
