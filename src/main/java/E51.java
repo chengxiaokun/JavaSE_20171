fd2bc15257700562b93f6fe06719c58af01ff391
mingfei <mingfei.net@gmail.com>
2017-03-14 17:17:22 +0800

    Update

---
+++ src/main/java/E51.java
@@ -0,0 +1,15 @@
+/**
+ * Created by mingfei.net@gmail.com
+ * 3/14/17 16:39
+ */
+
+// 斐波那契数列
+// 1,1,2,3,5,8,13,21,34,... f(n)
+public class E51 {
+    public static void main(String[] args) {
+        /*
+            f(n) = 1 (n=1, n=2)
+            f(n) = f(n-2) + f(n - 1) (n > 2)
+         */
+    }
+}
