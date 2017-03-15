62113c7d70fc72a2d0fde1d2d1314327610147d9
mingfei <mingfei.net@gmail.com>
2017-03-15 15:57:33 +0800

    Update

--- src/main/java/E51.java
+++ src/main/java/E51.java
@@ -6,10 +6,29 @@
 // 斐波那契数列
 // 1,1,2,3,5,8,13,21,34,... f(n)
 public class E51 {
+
+    /**
+     * 求 Fibonacci 数列的第 n 项
+     *       f(n) = 1 (n=1, n=2)
+     *       f(n) = f(n-2) + f(n-1) (n > 2)
+     *
+     *       recursive call 递归调用
+     *
+     *       非递归？
+     *
+     * @param n 数列的第 n 项
+     * @return Fibonacci(n)
+     */
+    private int fibonacci(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        } else {
+            return fibonacci(n - 2) + fibonacci(n - 1);
+        }
+    }
+
     public static void main(String[] args) {
-        /*
-            f(n) = 1 (n=1, n=2)
-            f(n) = f(n-2) + f(n - 1) (n > 2)
-         */
+        E51 e51 = new E51();
+        System.out.println(e51.fibonacci(4));
     }
 }
---
+++ src/main/java/E52.java
@@ -0,0 +1,37 @@
+/**
+ * Created by mingfei.net@gmail.com
+ * 3/15/17 15:44
+ */
+
+// 求解汉诺塔 n 圆盘移动步骤
+/*
+    Hanoi:
+
+        |       |       |
+        |       |       |
+        |       |       |
+        |       |       |
+       [ ]      |       |
+      [   ]     |       |
+      -----   -----   ------
+        A       B       C
+
+1. 每次移动一个
+2. 小的在大的上面
+N = 2
+A - B
+A - C
+B - C
+     */
+
+public class E52 {
+
+    private void hanoi(?) {
+        // ...
+    }
+
+    public static void main(String[] args) {
+        E52 e52 = new E52();
+        e52.hanoi(?);
+    }
+}
