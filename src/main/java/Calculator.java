7a5a2e5136c41c34e80d2283c013e12b21693604
mingfei <mingfei.net@gmail.com>
2017-03-15 14:15:53 +0800

    Update

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,13 @@
+/**
+ * Created by mingfei.net@gmail.com
+ * 3/15/17 10:34
+ */
+public class Calculator {
+    private int x;
+    private int y;
+
+    // x + y
+    public int add() {
+        return x + y;
+    }
+}
---
+++ src/main/java/Vehicle.java
@@ -0,0 +1,15 @@
+/**
+ * 交通工具类
+ *
+ * Created by mingfei.net@gmail.com
+ * 3/15/17 14:14
+ */
+public class Vehicle {
+
+    // 1. 3 fields
+
+    // 2.1 constructor
+
+    // 2.2 3 methods
+
+}
