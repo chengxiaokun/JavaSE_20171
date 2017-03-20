2e5a9a24e6d22b66a970b9d73e9f9ae13b5765db
mingfei <mingfei.net@gmail.com>
2017-03-20 17:04:24 +0800

    Update

--- src/main/java/java1702/javase/exercise/E34.java
+++ src/main/java/java1702/javase/exercise/E34.java
@@ -1,5 +1,7 @@
 package java1702.javase.exercise;
 
+import java.util.Random;
+
 /**
  * Created by mingfei.net@gmail.com
  * 3/20/17 15:46
@@ -7,6 +9,26 @@ package java1702.javase.exercise;
  */
 // 随机生成 [1,20] 随机数 10000 次
 // java.lang.Math  java.util.Random
+// util - utility 实用工具
 
+// 效率比较  分布比较
+
+// Random 随机
 public class E34 {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        long start = System.nanoTime(); // 返回系统当前时间毫微秒数
+        // 1490000302941  1/1000 second 1纳秒 = 1*10^-9 秒
+        for (int i = 0; i < 1000000; i++) {
+            int r = (int) (Math.random() * 20) + 1;
+//            int r = random.nextInt(20) + 1;
+//            System.out.println(r);
+        }
+        long end = System.nanoTime();
+
+//        System.out.println(end - start);
+
+        System.out.println(System.currentTimeMillis());
+    }
 }
--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -1,9 +1,32 @@
 package test;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by mingfei.net@gmail.com
  * 3/20/17 13:10
  * JavaSE_20171
  */
 public class Test {
+    public static void main(String[] args) throws IOException {
+        BufferedImage bufferedImage = ImageIO.read(new File("doc/1.png"));
+        int[][] matrix = new int[bufferedImage.getHeight()][bufferedImage.getWidth()];
+
+//        for (int i = 0; i < bufferedImage.getWidth(); i++) {
+//            for (int j = 0; j < bufferedImage.getHeight(); j++) {
+//                System.out.print(bufferedImage.getRGB(i, j) + "\t");
+//            }
+//            System.out.println();
+//        }
+
+        Color color = new Color(bufferedImage.getRGB(0, 0));
+        System.out.println(color.getAlpha());
+        System.out.println(color.getRed());
+        System.out.println(color.getGreen());
+        System.out.println(color.getBlue());
+    }
 }
