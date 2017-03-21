15f69b989c9ba78bc25da7594e1341d0dc5c4a3c
mingfei <mingfei.net@gmail.com>
2017-03-21 15:22:58 +0800

    Update

--- src/main/java/java1702/javase/exercise/E16.java
+++ src/main/java/java1702/javase/exercise/E16.java
@@ -18,5 +18,13 @@ public class E16 {
         .
         9 18 ... 81
          */
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (i >= j) {
+                    System.out.print((i + 1) * (j + 1) + "\t");
+                }
+            }
+            System.out.println();
+        }
     }
 }
