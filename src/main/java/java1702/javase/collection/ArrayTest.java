ec13be73b0599bd8bd8e39c9d5a30b4bcc520d9f
mingfei <mingfei.net@gmail.com>
2017-03-27 16:14:20 +0800

    Update

--- src/main/java/java1702/javase/collection/ArrayTest.java
+++ src/main/java/java1702/javase/collection/ArrayTest.java
@@ -13,14 +13,21 @@ public class ArrayTest {
         strings[0] = "test...";
         System.out.println(strings[0]); // 下标 索引 index
 
-        boolean[] booleans = new boolean[100];
+        boolean[] booleans = new boolean[10000];
+        booleans[0] = true;
         System.out.println(booleans[99]); // ?
 
         int[] ints = {1, 2, 3, 4, 5, 6, 67777, 888};
         System.out.println(ints[6]);
 
-        double[] doubles = new double[10000];
-        System.out.println(doubles.length);
+        double[] doubles = new double[1000];
+        for (int i = 0; i < doubles.length; i++) {
+            doubles[i] = i + 1;
+        }
+        System.out.println("--->" + doubles[doubles.length - 1]);
+        for (int i = 0; i < doubles.length; i++) {
+            System.out.println(doubles[i]);
+        }
 
 
         ArrayTest[] arrayTests = {new ArrayTest(), new ArrayTest(), new ArrayTest()};
@@ -28,5 +35,23 @@ public class ArrayTest {
         System.out.println(arrayTests[0]); // java1702.javase.collection.ArrayTest@
         System.out.println(arrayTests[1]); // ?
         System.out.println(arrayTests[2]); // ?
+        System.out.println(arrayTests.length);
+
+        String[] months = {"Jan", "Feb", "Mar", "Apr", "May"};
+
+        // itar + tab
+        for (int i = 0; i < months.length; i++) {
+            String month = months[i];
+            System.out.println(month);
+        }
+
+        System.out.println("-------------");
+
+        // iter + tab
+        for (String month : months) {
+            System.out.println(month);
+        }
+
+        int[] monthDays = {31, 28};
     }
 }
