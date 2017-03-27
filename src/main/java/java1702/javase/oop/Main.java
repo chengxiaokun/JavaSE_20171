8dd4084123d628cfa1c6bf51100d4f644c8da0b7
mingfei <mingfei.net@gmail.com>
2017-03-27 11:01:02 +0800

    Update

---
+++ src/main/java/java1702/javase/oop/Main.java
@@ -0,0 +1,61 @@
+package java1702.javase.oop;
+
+/**
+ * Created by mingfei.net@gmail.com
+ * 3/27/17 10:34
+ * JavaSE_20171
+ */
+public class Main {
+    public static void main(String[] args) {
+        Foo f = new Foo("f");
+        changeReference(f); // It won't change the reference!
+        modifyReference(f); // It will modify the object that the reference variable "f" refers to!
+        System.out.println(f.s); // f ? b
+    }
+
+    /*
+        f ------> obj f
+        a ---------\/
+        b ------> obj b
+
+        f ------> obj f > c
+        c ---------^
+
+     */
+    public static void changeReference(Foo a) {
+        Foo b = new Foo("b");
+
+        // 1: stack: b
+        // 2: heap: object
+        // 3: b ---> object
+
+        a = b;
+        System.out.println("-->" + a.s); //  b
+    }
+
+    public static void modifyReference(Foo c) {
+        c.setAttribute("c");
+    }
+}
+
+class Foo {
+    public String s;
+
+    public Foo(String s) {
+        this.s = s;
+    }
+
+    public void setAttribute(String s) {
+        this.s = s;
+    }
+}
+
+/*
+f -------|
+         |
+a -------|---------|
+b--------|---------|
+-------- 01---------02---------03----04-05-----------
+    |    "f"   |      "b"        |      |    | |
+----------------------------------------------
+ */
--- src/main/java/java1702/javase/oop/PrimitiveTest.java
+++ src/main/java/java1702/javase/oop/PrimitiveTest.java
@@ -10,29 +10,28 @@ public class PrimitiveTest {
     private String s;
 
     // java.lang.String FQN
-    private static void method(PrimitiveTest test) { // 值传递
-        System.out.println("b: " + test.s); // hello
-        test.s = "hi";
-        System.out.println("c: " + test.s); // hi
+    private static void method(Integer integer) { // 值传递
+        System.out.println("b: " + integer); // 0
+        integer = new Integer(1);
+        System.out.println("c: " + integer); // 1
     }
 
     public static void main(String[] args) {
-        PrimitiveTest primitiveTest = new PrimitiveTest();
-        primitiveTest.s = "hello"; // 0
-        System.out.println("a: " + primitiveTest.s); // hello
-        method(primitiveTest);
-        System.out.println("d: " + primitiveTest.s); // hello
+        Integer integer = new Integer(0);
+        System.out.println("a: " + integer); // 0
+        method(integer);
+        System.out.println("d: " + integer); // 0
     }
 }
 /*
 值传递：地址传递
-        形式参数 test -------------------------------|
-                                                    |
+        形式参数 test ----------------------------> address
+                                                     ^
         实际参数 primitiveTest -------------------> address
 
 
-值传递
+值传递：值本身
         形式参数 j -------------------> address2 0
-                                                |
+                                                ^
         实际参数 i -------------------> address1 0
  */
