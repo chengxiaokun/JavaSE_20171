d2fc40fa7dbaf4b74a1c3350735eea0052e22730
mingfei <mingfei.net@gmail.com>
2017-03-29 09:15:55 +0800

    Update

--- src/main/java/java1702/javase/collection/StringTest.java
+++ src/main/java/java1702/javase/collection/StringTest.java
@@ -7,7 +7,7 @@ package java1702.javase.collection;
  */
 public class StringTest {
     public static void main(String[] args) {
-        String s = "helloworld";
+        String s = "hello world! test";
         // match 匹配\ [mætʃ]
         System.out.println(s.matches("\\w+")); // JavaScript
 //        System.out.println(s.contains("he"));
@@ -16,8 +16,17 @@ public class StringTest {
         // repalce 取代\ [rɪ'pleɪs]
 
 //        System.out.println(s.replace("l", "a"));
-        System.out.println(s.replaceAll("l", "a"));
+        System.out.println(s.replaceAll("ll", "a"));
 //        System.out.println(s.replaceAll("[l]+", "a"));
-        System.out.println(s.replaceFirst("l", "a"));
+        System.out.println(s.replaceFirst("ll", "a"));
+
+        // split 分裂\ [splɪt]
+        String[] strings = s.split("\\s+"); // space 空格
+        System.out.println(strings.length); // 10 12 11
+        for (String string : strings) {
+            System.out.println(string);
+        }
+
+        System.out.println(s.substring(6, 11));
     }
 }
