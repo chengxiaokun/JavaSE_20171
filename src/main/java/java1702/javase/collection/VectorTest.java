1af21fb6bca5f92cd857b339a5ad833e46ed58a6
mingfei <mingfei.net@gmail.com>
2017-03-29 14:39:48 +0800

    Update

--- src/main/java/java1702/javase/collection/VectorTest.java
+++ src/main/java/java1702/javase/collection/VectorTest.java
@@ -1,5 +1,7 @@
 package java1702.javase.collection;
 
+import com.sun.corba.se.impl.orbutil.ObjectStreamClassUtil_1_3;
+
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Vector;
@@ -32,11 +34,21 @@ public class VectorTest {
         System.out.println(vector.capacity());
         // capacity 容量,\ 能力,\ 容积
 
+        Vector<Integer> integers = new Vector<>();
+        integers.add(1);
+        integers.add(10);
+        integers.add(100);
+        System.out.println(integers.size());
+        System.out.println(integers.capacity());
+        for (Integer integer : integers) {
+            System.out.println(integer);
+        }
+
         Set<String> strings = new HashSet<>();
         strings.add("a");
         strings.add("aa");
         strings.add("aaa");
         strings.add("aaa");
-        System.out.println(strings.size());
+//        System.out.println(strings.size());
     }
 }
