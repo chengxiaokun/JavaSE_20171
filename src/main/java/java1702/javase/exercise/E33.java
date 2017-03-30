066db5806ae608a1268fdc8660e954019ef69a6b
mingfei <mingfei.net@gmail.com>
2017-03-30 09:37:13 +0800

    Update

--- src/main/java/java1702/javase/exercise/E33.java
+++ src/main/java/java1702/javase/exercise/E33.java
@@ -7,6 +7,7 @@ package java1702.javase.exercise;
 
  打印出杨辉三角形（要求打印出 n 行如下图）
      1
+    1 1
    1 2 1
   1 3 3 1
  1 4 6 4 1
@@ -14,4 +15,13 @@ package java1702.javase.exercise;
 
  */
 public class E33 {
+    public static void main(String[] args) {
+        int[] l1 = {1};
+        int[] l2 = {1, 1};
+        int[] l3 = {1, 2, 1};
+        int[] l4 = {1, 3, 3, 1};
+//        int[] l5 = {1, 4, 6, 4, 1};
+        int[] l5 = new int[5];
+
+    }
 }
