7ac41916c8bcfa5a35b6509878db47531a4695fd
mingfei <mingfei.net@gmail.com>
2017-03-31 08:59:07 +0800

    Update

--- src/main/java/java1702/javase/collection/ArrayVector.java
+++ src/main/java/java1702/javase/collection/ArrayVector.java
@@ -11,38 +11,54 @@ public class ArrayVector { // 使用数组来模拟一个向量 DynamicArray
 
     private static final int DEFAULT_CAPACITY = 10;
     private String[] strings;
+    private int capacity;
     private int size;
 
     public ArrayVector() {
         strings = new String[DEFAULT_CAPACITY];
+        capacity = DEFAULT_CAPACITY;
     }
 
     public ArrayVector(int initialCapacity) {
         strings = new String[initialCapacity];
+        capacity = initialCapacity;
     }
 
     public void add(String string) {
+        if (size == capacity) {
+            String[] newStrings = new String[capacity * 2];
+            capacity *= 2;
+            System.arraycopy(strings, 0, newStrings, 0, strings.length);
+            strings = newStrings;
+        }
         strings[size] = string;
         size++;
     }
 
     public String get(int index) {
+        if (index >= size) { // Exception
+            System.out.println("error.");
+            System.exit(0);
+        }
         return strings[index];
     }
 
+    public int size() {
+        return size;
+    }
+
+    public int capacity() {
+        return capacity;
+    }
+
     public static void main(String[] args) {
-        // void add(String element)
         // String remove(int index)
         // String set(int index, String element)
-        // String get(int index)
-        // int size()
-        // int capacity()
-
-        ArrayVector arrayVector = new ArrayVector();
+        ArrayVector arrayVector = new ArrayVector(2);
         arrayVector.add("hello");
         arrayVector.add("hi");
-        System.out.println(arrayVector.get(0)); // hello
-        System.out.println(arrayVector.get(1)); // hi
+        System.out.println(arrayVector.size());
+        System.out.println(arrayVector.capacity());
     }
 
     /*
