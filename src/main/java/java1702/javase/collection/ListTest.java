8d75261318e8d60bb659dd943ccab927f2929d77
mingfei <mingfei.net@gmail.com>
2017-03-31 14:00:57 +0800

    Update

--- src/main/java/java1702/javase/collection/ListTest.java
+++ src/main/java/java1702/javase/collection/ListTest.java
@@ -7,7 +7,7 @@ import java.util.*;
  * 3/31/17 11:28
  * JavaSE_20171
  */
-public class ListTest {
+public class ListTest extends ArrayList {
     public static void main(String[] args) {
 //        Iterable 可迭代的
 //        Collection 集合
@@ -16,6 +16,32 @@ public class ListTest {
 //        Map 映射
 //        Vector
 
-        ArrayList
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("hi");
+        arrayList.add("hello");
+        arrayList.add("hello");
+        System.out.println(arrayList.size());
+        System.out.println(arrayList.get(0));
+        System.out.println(arrayList.contains("hello"));
+        System.out.println(arrayList.indexOf("hello"));
+        System.out.println(arrayList.lastIndexOf("hello"));
+        System.out.println(arrayList.remove(1));
+        System.out.println(arrayList.get(1));
+        System.out.println(arrayList.set(1, "test"));
+        System.out.println(arrayList.isEmpty());
+//        arrayList.clear();
+        System.out.println(arrayList.isEmpty());
+        System.out.println("--->" + arrayList);
+        Object[] objects = arrayList.toArray();
+
+        ListTest listTest = new ListTest();
+        for (int i = 0; i < 10; i++) {
+            listTest.add("" + i);
+        }
+        listTest.removeRange(3, 5);
+        for (Object o : listTest) {
+//            System.out.println(o);
+        }
+
     }
 }
