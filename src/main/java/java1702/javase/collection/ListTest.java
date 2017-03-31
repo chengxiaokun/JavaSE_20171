a95bb52ddcad4eb0c5a25d159eb91a7b1fd2fe16
mingfei <mingfei.net@gmail.com>
2017-03-31 11:53:26 +0800

    Update

---
+++ src/main/java/java1702/javase/collection/ListTest.java
@@ -0,0 +1,21 @@
+package java1702.javase.collection;
+
+import java.util.*;
+
+/**
+ * Created by mingfei.net@gmail.com
+ * 3/31/17 11:28
+ * JavaSE_20171
+ */
+public class ListTest {
+    public static void main(String[] args) {
+//        Iterable 可迭代的
+//        Collection 集合
+//        List 列表
+//        Set 集合
+//        Map 映射
+//        Vector
+
+        ArrayList
+    }
+}
