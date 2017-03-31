239a8d164a5e9def130a0b7fc5cb311eb80c050e
mingfei <mingfei.net@gmail.com>
2017-03-31 10:10:58 +0800

    Update

--- src/main/java/java1702/javase/collection/StringCopyTest.java
+++ src/main/java/java1702/javase/collection/StringCopyTest.java
@@ -9,16 +9,18 @@ import java.util.Arrays;
  */
 public class StringCopyTest {
     public static void main(String[] args) {
-        int[] srcArray = {1, 2, 3, 4, 5, 6};
-        int[] destArray = new int[10];
+//        int[] srcArray = {1, 2, 3, 4, 5, 6};
+//        int[] destArray = new int[10];
+//
+//        // src - source 源\ [sɔːs]
+//        // dest - destination 目的地\ [,destɪ'neɪʃ\(ə\)n]
+//        System.out.println(Arrays.toString(destArray));
+//        System.arraycopy(srcArray, 1, destArray, 6, 4);
+//        System.out.println(Arrays.toString(destArray));
 
-        // src - source 源\ [sɔːs]
-        // dest - destination 目的地\ [,destɪ'neɪʃ\(ə\)n]
-        System.out.println(Arrays.toString(destArray));
-        System.arraycopy(srcArray, 1, destArray, 6, 4);
-        System.out.println(Arrays.toString(destArray));
-
+        // String remove(int index)
 
         String[] strings = {"a", "b", "c", "d", null, null, null, null, null, null};
+        System.arraycopy(strings, 0, strings, 0, 0);
     }
 }
