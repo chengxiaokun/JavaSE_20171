e712ccc4379efb34cf9898902c980286c7be3bd2
mingfei <mingfei.net@gmail.com>
2017-04-11 11:30:46 +0800

    Update

--- src/main/java/java1702/javase/collection/MyLinkedList.java
+++ src/main/java/java1702/javase/collection/MyLinkedList.java
@@ -17,8 +17,18 @@ public class MyLinkedList {
         MyLinkedList myLinkedList = new MyLinkedList();
         myLinkedList.add("a");
         myLinkedList.add("b");
+        myLinkedList.add("c");
         System.out.println(myLinkedList.first());
         System.out.println(myLinkedList.last());
+        System.out.println(myLinkedList.get(1));
+    }
+/*
+   ----------[0]---[?]---[?]---[?].............[n?]
+ */
+
+    public String get(int index) {
+        // TODO: 4/11/17
+        return null;
     }
 
     public boolean add(String element) {
@@ -27,7 +37,9 @@ public class MyLinkedList {
             first = node;
             last = node;
         } else {
-
+            Node node = new Node(element, last, null);
+            last.next = node;
+            last = node;
         }
         return true;
     }
