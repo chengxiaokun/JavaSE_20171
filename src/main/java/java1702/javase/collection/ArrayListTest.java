9fb074ced9866332802d6995ea9b5c413d71ba7f
mingfei <mingfei.net@gmail.com>
2017-04-11 08:58:44 +0800

    Update

---
+++ src/main/java/java1702/javase/collection/ArrayListTest.java
@@ -0,0 +1,35 @@
+package java1702.javase.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mingfei.net@gmail.com
+ * 4/11/17 08:39
+ * https://github.com/thu/JavaSE_20171
+ */
+public class ArrayListTest {
+    public static void main(String[] args) {
+        List<String> strings1 = new ArrayList<>();
+        strings1.add("a");
+        strings1.add("b");
+        List<String> strings2 = new ArrayList<>();
+        strings2.add("a");
+        strings2.add("b");
+        strings2.add("c");
+// [a, c]
+        System.out.println(strings1.equals(strings2));
+
+        List<String> strings3 = strings2.subList(1, 3);
+        System.out.println(strings3);
+
+        System.out.println(strings3.containsAll(strings1));
+
+        strings2.removeAll(strings3);
+        System.out.println(strings2);
+
+        // retain 保留\ [rɪ'teɪn]
+        strings3.retainAll(strings2);
+        System.out.println(strings3);
+    }
+}
---
+++ src/main/java/java1702/javase/collection/E1.java
@@ -0,0 +1,11 @@
+package java1702.javase.collection;
+
+/**
+ * Created by mingfei.net@gmail.com
+ * 4/10/17 17:16
+ * https://github.com/thu/JavaSE_20171
+ */
+
+//
+public class E1 {
+}
